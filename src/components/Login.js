fa7abfbde8401aa1fe7a623f93e7ea6518d41b4a
Mohammed Talha <self-codex@users.noreply.github.com>
2021-01-19 23:49:03 +0500

    initial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./login.css";
+import Linkedin from "../images/linkedin_hack.png";
 
 function Login() {
   return (
-    <div>
-      <h1>login page</h1>
+    <div className="login">
+      <img src={Linkedin} alt="linkedin" />
+      <form autoComplete="off" className="login__form">
+        <input
+          type="text"
+          placeholder="Enter your name (only require registration"
+          name="name"
+        />
+        <input type="text" name="profile" placeholder="Enter profile url" />
+        <input type="email" name="email" placeholder="Ente user email" />
+        <input type="password" name="password" placeholder="Enter Password" />
+        <button className="sign__btn" type="submit">
+          Sign in
+        </button>
+      </form>
     </div>
   );
 }
